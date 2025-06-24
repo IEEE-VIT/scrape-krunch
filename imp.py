e45ba9a5f3a7172c1e698a3bc6e7f8fe4f3f3486
raptor7197 <vamsikrishna75836@gmail.com>
2025-06-25 02:46:49 +0530

    still basic wip

--- imp.py
+++ imp.py
@@ -1,16 +1,42 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import time
+import os
+from ollama import chat
+from ollama import ChatResponse
 
 
 
 def get_stuff():
     html=requests.get("https://idrw.org/")
-    soup=bs(html.content,"xml")
-    items=soup.find_all("p")
-    print("items\n")
-    for item in items:
+    soup=bs(html.text,"xml")
+    headings = soup.find_all("h2")
+    # para=soup.find("div", class_="art-postcontent clearfix")
+    para=soup.find_all("article")
+    for item in headings:
         print(item.text)
-    articles = []
+        print(para[2].text)
+        print(item.find_next("p").text if item.find_next("p") else "no para ")
+        
+        
+    response: ChatResponse = chat(model='llama3.2', messages=[
+    {
+        'role': 'user',
+        'content': f"Summarize this : {para[2].text}",
+    },
+    ])
+    print("\n\n\n\n\n\n\n\n")
+    print(response['message']['content'])
+    print(response.message.content)
+
+    # print("items\n")
+    # for item in headings:
+    #     print(item.text)
+    # for h in para:
+    #     print(h.text)
+
+
+    print(os.cpu_count())
 
 get_stuff()
+    # or access fields directly from the response object
